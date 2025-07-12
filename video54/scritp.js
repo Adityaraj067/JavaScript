ef484ba4f45957c58d45f5647fd083ffb9fa742e
adityaraj067 <adityarajpandey62003@gmail.com>
2025-07-12 14:29:41 +0530

    variable

---
+++ video54/scritp.js
@@ -0,0 +1,19 @@
+
+alert("hello");
+console.log("code is running")
+
+// var a= prompt("enter your number")
+// var isTrue = confirm("are you blast your computer")
+
+// if(isTrue){
+//     console.log("computer is blasting")
+// }
+// else{
+//     console.log("computer is  not blasting")
+// }     
+// console.log("your numebr is "+a)  
+
+document.title = "hey i am good"
+
+document.body.style.backgroundColor = "red"
+
---
+++ video55/script.js
@@ -0,0 +1,25 @@
+console.log("hey this is aditya raj pandey")
+
+var a = 5;
+a = a+1
+var b = 5;
+{
+    let a = 534;
+    console.log(a)
+}
+console.log(a)
+
+let x= "aditya";
+let y = 3242;
+let z = 3.333;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    name:"afaf",
+    "afda akf a":"af"
+}
+console.log(o)
