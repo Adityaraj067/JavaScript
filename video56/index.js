46d0208f199f163a5defd08ba0345fa66d357dfd
adityaraj067 <adityarajpandey62003@gmail.com>
2025-07-12 15:19:03 +0530

    if else

---
+++ video56/index.js
@@ -0,0 +1,9 @@
+console.log("hello this is condition tutorial    ")
+
+let age  = 5;
+if(age>18){
+    console.log("you can drive....")
+}
+else{
+    console.log('you can not drive...')
+}                            
